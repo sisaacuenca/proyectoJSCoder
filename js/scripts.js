fe0c096f0a4aee1efd5359e9af80a64cacae0aea
Santiago Isaac Cuenca <santiagoisaaccuenca@gmail.com>
2022-06-06 20:57:16 -0300

    retocando logica y funciones

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,18 @@
 "use strict";
 var carrito = 0;
-
+const valores = [500, 900, 1200, 350, 2900, 5000];
+var opcion;
+function elegirProducto() {
+  opcion = prompt("elija producto del 1 al 6");
+  opcion -= 1;
+  addCarrito(valores[opcion]);
+}
 function addCarrito(valor) {
   carrito = carrito + valor;
   return carrito;
+}
+function verCarrito() {
+  alert("el monto actual del carrito es de :  " + carrito);
 }
 function finalizarCompra() {
   alert("el valor de la compra es:" + carrito);
