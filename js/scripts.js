5e41dc597a969062b91a90fe77f1458244ce98ee
Santiago Isaac Cuenca <santiagoisaaccuenca@gmail.com>
2022-06-20 15:13:26 -0300

    interactuando con DOM y eventos

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,42 @@
 const productos = [];
 const carrito = [];
 let opcion = 0;
+const listProductos = document.getElementById("listProductos");
+const add1 = document.querySelector("#add1");
+const verCarro = document.querySelector("#carrito");
+class Producto {
+  constructor(id, nombre, precio) {
+    this.id = id;
+    this.nombre = nombre;
+    this.precio = precio;
+  }
+  precioFinal() {
+    return parseFloat((this.precio * IVA).toFixed(2));
+  }
+}
+function generarProductos() {
+  productos.push(new Producto(0001, "MOUSE GENIUS", 500));
+  productos.push(new Producto(0002, "TECLADO LOGITECH", 900));
+  productos.push(new Producto(0003, "COOLER AUREOX", 1200));
+  productos.push(new Producto(0004, "PARLANTES NOGA", 350));
+  productos.push(new Producto(0005, "PROCESADOR INTEL CELERON", 2900));
+  productos.push(new Producto(0006, "GEFORCE GT1030", 5000));
+}
+function generarCarrito() {
+  carrito.push(new Producto(0001, "MOUSE GENIUS", 500));
+  carrito.push(new Producto(0002, "TECLADO LOGITECH", 900));
+  carrito.push(new Producto(0003, "COOLER AUREOX", 1200));
+}
 
+generarProductos();
+generarCarrito();
+
+function mostrarProductos() {
+  listProductos.innerHTML = "";
+  for (const producto of productos) {
+    listProductos.innerText = producto;
+  }
+}
 function elegirProducto() {
   opcion = prompt("elija producto del 1 al 6");
   opcion -= 1;
@@ -13,18 +48,21 @@ function addProducto() {
   let nombre = prompt("ingrese nombre del producto");
   let precio = parseInt(prompt("ingrese precio del producto"));
   productos.push(new Producto(id, nombre.toUpperCase(), precio));
+  return productos;
+  console.log(productos);
 }
 
-class Producto {
-  constructor(id, nombre, precio) {
-    this.id = id;
-    this.nombre = nombre;
-    this.precio = precio;
+function addCarrito(addId) {
+  const existe = productos.some((producto) => producto.id === addId);
+  if (existe) {
+    carrito.push(productos.find((producto) => producto.id === addId));
+    console.log();
+  } else {
+    console.log("no existe el producto solicitado");
   }
-  precioFinal() {
-    return parseFloat((this.precio * IVA).toFixed(2));
-  }
+  verCarrito();
 }
+
 function listarProductos() {
   productos.forEach((producto) => {
     console.table(producto);
@@ -40,29 +78,7 @@ function buscarProducto() {
   const buscado = productos.find((producto) => producto.id === idBuscada);
   console.log(buscado);
 }
-function addCarrito() {
-  let idAgregar = parseInt(prompt("ingrese la ID del producto a agregar"));
-  const existe = productos.some((producto) => producto.id === idAgregar);
-  if (existe) {
-    carrito.push(productos.find((producto) => producto.id === idAgregar));
-  } else {
-    console.log("no existe el producto solicitado");
-  }
-}
 
-function generarProductos() {
-  productos.push(new Producto(0001, "MOUSE GENIUS", 500));
-  productos.push(new Producto(0002, "TECLADO LOGITECH", 900));
-  productos.push(new Producto(0003, "COOLER AUREOX", 1200));
-  productos.push(new Producto(0004, "PARLANTES NOGA", 350));
-  productos.push(new Producto(0005, "PROCESADOR INTEL CELERON", 2900));
-  productos.push(new Producto(0006, "GEFORCE GT1030", 5000));
-}
-function generarCarrito() {
-  carrito.push(new Producto(0001, "MOUSE GENIUS", 500));
-  carrito.push(new Producto(0002, "TECLADO LOGITECH", 900));
-  carrito.push(new Producto(0003, "COOLER AUREOX", 1200));
-}
 function crearID() {
   return parseInt(Math.random() * 10000);
 }
